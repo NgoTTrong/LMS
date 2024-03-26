25512dc00219843340660219b9dd79d7be5f4b16
Aura Mac device <auramac@Mac-mini-cua-Aura.local>
2024-03-26 11:41:39 +0700

    update backend endpoint

--- services/api/endpoint.ts
+++ services/api/endpoint.ts
@@ -1,4 +1,4 @@
-const apiEndpoint = "https://toeic-mastery-api.onrender.com/";
+const apiEndpoint = "https://toeic-backend-api.onrender.com";
 // const apiEndpoint = "http://localhost:8080";
 
 export { apiEndpoint };
