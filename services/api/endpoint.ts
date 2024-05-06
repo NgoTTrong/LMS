bfce8771911e1816aefab5ed0a8c87d42f246f79
Trong Ngo <trongngo08082002@gmail.com>
2024-05-06 11:56:30 +0700

    fix

--- services/api/endpoint.ts
+++ services/api/endpoint.ts
@@ -1,4 +1,4 @@
-// const apiEndpoint = "https://toeic-backend-api.onrender.com";
-const apiEndpoint = "http://localhost:8080";
+const apiEndpoint = "https://toeic-backend-api.onrender.com";
+// const apiEndpoint = "http://localhost:8080";
 
 export { apiEndpoint };
